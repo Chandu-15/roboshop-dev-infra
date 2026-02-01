28b054cf20e954f21ab0787ff68e9b0f11f07e4e
Chandana <rebbachandana54@gmail.com>
2026-02-01 16:57:16 +0000

    s

--- 80-frontend-alb/parameters.tf
+++ 80-frontend-alb/parameters.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "frontend_alb_listener"{
     name="/${var.project_name}/${var.environment}/frontend_alb_listener"
     type="String"
-    value=aws_lb.frontend_alb.arn
+    value=aws_lb_listener.frontend_alb.arn
     overwrite = true
 }
