8543ff6efd273d747ffedae57840313d26e09fbc
Chandana <rebbachandana54@gmail.com>
2026-02-01 15:19:13 +0000

    s

--- 21-vpn/data.tf
+++ 21-vpn/data.tf
@@ -3,7 +3,7 @@ data "aws_ami" "joindevops"{
     most_recent=true
     filter {
         name="name"
-        values=["OpenVPN Access Server Community Image-fe8020db-5343-4c43-9e65-5ed4a825c931"]
+        values=[OpenVPN Access Server Community Image*]
     }
     filter {
         name="root-device-type"
